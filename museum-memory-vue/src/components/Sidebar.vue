fd1c1683a038633a0b087d8fa765e775f90645a7
zlq312 <zlq312@gmail.com>
2019-07-20 01:57:39 -0400

    init vue part

---
+++ museum-memory-vue/src/components/Sidebar.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+    <el-menu :default-openeds="['1', '3']">
+      <el-submenu index="1">
+        <template slot="title"><i class="el-icon-message"></i>展</template>
+        <el-menu-item-group>
+          <el-menu-item index="1-1">画</el-menu-item>
+          <el-menu-item index="1-2">雕塑</el-menu-item>
+          <el-menu-item index="1-3">当代艺术</el-menu-item>
+          <el-menu-item index="1-4">其他</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-menu-item index="2-1"><i class="el-icon-menu"></i>博物馆</el-menu-item>
+      <el-menu-item index="3-1"><i class="el-icon-setting"></i>人物</el-menu-item>
+    </el-menu>
+  </el-aside>
+</template>
+
+<script>
+
+export default {
+  name: 'Sidebar'
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.el-aside {
+  color: #333;
+}
+</style>
---
+++ museum-memory-vue/src/views/Home.vue
@@ -0,0 +1,59 @@
+<template>
+  <el-container style="height: 500px; border: 1px solid #eee">
+    <sidebar/>
+    <el-container>
+      <el-header style="text-align: right; font-size: 12px">
+        <el-dropdown>
+          <i class="el-icon-setting" style="margin-right: 15px"></i>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>View</el-dropdown-item>
+            <el-dropdown-item>Add</el-dropdown-item>
+            <el-dropdown-item>Delete</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+        <span>Tom</span>
+      </el-header>
+
+      <el-main>
+        <el-table :data="tableData">
+          <el-table-column prop="date" label="Date" width="140">
+          </el-table-column>
+          <el-table-column prop="name" label="Name" width="120">
+          </el-table-column>
+          <el-table-column prop="address" label="Address">
+          </el-table-column>
+        </el-table>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<style>
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    color: #333;
+  }
+</style>
+
+<script>
+  import Sidebar from '../components/Sidebar'
+
+  export default {
+    components: { Sidebar },
+    data() {
+      const item = {
+        date: '2016-05-02',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      };
+      return {
+        tableData: Array(20).fill(item)
+      }
+    }
+  };
+</script>
