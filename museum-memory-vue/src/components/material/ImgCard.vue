5381001987b327d4890909ce1737c69719df5af1
zlq312 <zlq312@gmail.com>
2019-08-16 15:13:33 -0400

    use vue material and start making Exhibition pages

---
+++ museum-memory-vue/src/components/material/ImgCard.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-card class="mx-auto" 
+    >
+        <v-img class="white--text" height="200px" :src="url">
+            <v-card-title class="align-end fill-height">{{ title }}</v-card-title>
+        </v-img>
+        <v-card-text>{{ text }}</v-card-text>
+        <v-card-actions v-if="actions">
+            <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+            </v-btn>
+            <v-btn text>Click</v-btn>
+            <v-btn outlined>Click</v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+<script>
+export default {
+    inheritAttrs: false,
+
+    props: {
+      url: {
+        type: String,
+        default: undefined
+      },
+      actions: {
+        type: Boolean,
+        default: false
+      },
+      title: {
+        type: String,
+        default: "Title"
+      },
+      text: {
+        type: String,
+        default: "Content"
+      }
+    }
+}
+</script>
---
+++ museum-memory-vue/src/views/Exhibition.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-container
+    fill-height
+    fluid
+    grid-list-xl
+  >
+    <v-layout wrap>
+      <v-flex
+        md12
+        sm12
+        lg4
+
+         v-for="exhibition in exhibitions"
+      >
+
+        <material-img-card
+          :url="exhibition.url"
+          :text="exhibition.desc"
+          :title="exhibition.name"
+        >
+        </material-img-card>
+
+      </v-flex>
+   
+     
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      exhibitions: [
+        {
+          name: "fangao",
+          desc: "first",
+          url : 'https://cdn.vuetifyjs.com/images/cards/docks.jpg'
+        },
+        {
+          name: "monai",
+          desc: "second",
+          url : 'https://cdn.vuetifyjs.com/images/cards/docks.jpg'
+        },
+        {
+          name: "fangao2",
+          desc: "first2",
+          url : 'https://cdn.vuetifyjs.com/images/cards/docks.jpg'
+        },
+        {
+          name: "fangao3",
+          desc: "first3",
+          url : 'https://cdn.vuetifyjs.com/images/cards/docks.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
